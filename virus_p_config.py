f00f0ef672f197e04372dcd5feded2f354f667e2
Briana Lane Indahl <blindahl@astro.as.utexas.edu>
2017-04-13 10:47:34 -0500

    debug

--- virus_p_config.py
+++ virus_p_config.py
@@ -4,13 +4,13 @@
 
 #reduction_wrapper_virusP.py should be run inside of a folder containing the date folder containing raw data
 
-redux_dir        = "VP_apr15_B1"	 	#name of the folder reduction is run 
-date_folder      = "20150417"		#date folder containing raw data 
+redux_dir        = "redux_20160404"	 	#name of the folder reduction is run 
+date_folder      = "20160404"		#date folder containing raw data 
 
 #Choose the spectral range of your VIRUS-P data 
-spec_range   	 = [3500,5850] 		#Spectral range of that nights observations in angstroms
+spec_range   	 = [4700,7200] 		#Spectral range of that nights observations in angstroms
 
-lines_file       = "/Users/Briana/Documents/Grad_School/VIRUS_P/apr15/lines.apr15"	#path to the lines file for this data set
+lines_file       = "/Users/Briana/Documents/Grad_School/VIRUS_P/VP_APR_2016/vp_new_04/lines.apr16"	#path to the lines file for this data set
 IFU_file 		 = "/Users/Briana/Documents/cure_v2/cure/config/IFUcen_VP2_27m.txt" #path to the IFUcen file for this data set
 
 CLEAN_AFTER_DONE = True 
@@ -25,16 +25,16 @@ CLEAN_AFTER_DONE = True
 #This should be a list of the objects names of standard star objects as written in the headers
 standard_frames  = ['Feige34',]
 #If you took sky frames, this should be a list of the object names of those frames as written in the headers
-sky_frames		 = ['M82_sky',]
+sky_frames		 = ['M82_sky2',]
 #This should be the object names of the science frames as written in the headers
-science_frames      = ['M82_F1',]	
+science_frames      = ['M82_F9',]	
 
 #####################
 # REDUCTION OPTIONS #
 #####################
 basic           = False		#run basic reduction (normalize, build mastertrace and masterarc)
 run_deformer    = False		#run deformer to map spectral traces and build wavelength solution for fiber extraction
-subsky          = True  	#run sky subtraction on sci images - Need to have run deformer, only runs on non-extracted spectra
+subsky          = True	 	#run sky subtraction on sci images - Need to have run deformer, only runs on non-extracted spectra
 fiberextract    = False	 	#extract spectra and save into fits file - Need to have run deformer
 makecube 		= False		#builds data cube out of fiber extracted image - Need to have run fiberextract
 collapseCube 	= False		#collapse data cube to make an image of a wavelength range of the users choice
@@ -44,7 +44,7 @@ collapseCube 	= False		#collapse data cube to make an image of a wavelength rang
 #------------#
 ref_line		= 5 			#[integer] arc line in line list that is the brightest but not blended (used as reference line)
 subDarks 		= False			#[True/False] If True darks will be subtracted from science images (default: False)
-rmCosmics	 	= True  		#[True/False] If True the program L.A.Cosmic is used to eliminate cosmic rays (default: True)
+rmCosmics	 	= False  		#[True/False] If True the program L.A.Cosmic is used to eliminate cosmic rays (default: True)
 
 #----------------------#
 # sky subtraction opts #
@@ -52,18 +52,19 @@ rmCosmics	 	= True  		#[True/False] If True the program L.A.Cosmic is used to el
 window_size 	= 250 			#[integer] Size (in image lines) of moving window for sky median
 sky_kappa 		=[3.5,3.5]		#[floatarray] Lower and upper kappa for final sky kappa-sigma clipping.
 smoothing 		= 5.0 			#[float] Smoothing factor for approximating spline.
-sn_thresh		= 5 			#[float] Minimum signal to noise to flag fiber as continuum and ignore it during sky generation.
+sn_thresh		= 15 			#[float] Minimum signal to noise to flag fiber as continuum and ignore it during sky generation.
+sky_scaling 	= 1 			#[float] Scale the sky by this factor before subtracting. (** only for when sky frames used **)
 
 #--------------------#
 # fiber extract opts #
 #--------------------#
 wl_resample 	 = True 		#[True/False] If True it will resample in wavelength, Does not resample in wavelength if False 
-use_ap_corr	 	 = False		# Apply aperture correction using the fiber model (slow).
+#use_ap_corr	 	 = False		# Apply aperture correction using the fiber model (slow).
 
 #----------------#
 # make cube opts #
 #----------------#
-#these need to be the length of the science_frame list 
+#these need to be the length of the number of dithers you have (the science_frame list) 
 airmass_lis 	= [1.0,]
 seeing_lis 		= [3.0,]
 
