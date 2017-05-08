bef244e34e1592b9b3b1cddc2df32f266a806f56
Briana Lane Indahl <blindahl@astro.as.utexas.edu>
2017-05-08 13:20:23 -0500

    rename error

--- fiberextract_dithers.py
+++ fiberextract_dithers.py
@@ -2,6 +2,7 @@ import string
 import subprocess
 import glob
 import os.path as op
+import pyfits 
 
 #************************#
 # User Defined Variables #
@@ -26,10 +27,7 @@ wl_higher = 7200
 # Find files and run fiberextract #
 #*********************************#
 
-if extract_error:
-	sci_im_list = glob.glob('e.Sp*.fits')
-else:
-	sci_im_list = glob.glob('Sp*.fits')	
+sci_im_list = glob.glob('Sp*.fits')	
 
 filelis = []
 for s in sci_im_list:
@@ -41,6 +39,7 @@ print 'Files found: '+str(filelis)
 for i in range(len(filelis)):
 	if extract_error:
 		bash_com = op.join(curebin_path,'fiberextract -d pes'+str(prefix)+str(filelis[i])+'.dist -f pes'+str(prefix)+str(filelis[i])+'.fmod -c -r 1,246 -l '+str(wl_lower)+','+str(wl_higher)+' e.Spes'+str(prefix)+str(filelis[i])+'.fits')
+		shutil.move('Fee.Spes'+str(prefix)+str(filelis[i])+'.fits','e.FeSpes'+str(prefix)+str(filelis[i])+'.fits')
 	else:
 		bash_com = op.join(curebin_path,'fiberextract -d pes'+str(prefix)+str(filelis[i])+'.dist -f pes'+str(prefix)+str(filelis[i])+'.fmod -c -r 1,246 -l '+str(wl_lower)+','+str(wl_higher)+' Spes'+str(prefix)+str(filelis[i])+'.fits')
 	#$CUREBIN/fiberextract -d pesvp0068.dist -f pesvp0068.fmod -c -r 1,246 -l [3500,5800] Spesvp0068.fits 
